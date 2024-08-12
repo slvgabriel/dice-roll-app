2865ed33c7e755df5569493700cc480c7e6da829
Gabriel Silva <109912774+slvgabriel@users.noreply.github.com>
2024-08-12 14:10:03 -0300

    main

---
+++ index.js
@@ -0,0 +1,41 @@
+function rollDice() {
+    // returning values
+    const numOfDice = document.getElementById("numOfDice").value;
+    const diceResults = document.getElementById("diceResults");
+    const diceImgs = document.getElementById("diceImgs");
+    const values = [];
+    const images = [];
+
+    //
+    if (numOfDice > 20) {
+        window.alert("You can roll up to 20 dices only!");
+        document.getElementById("numOfDice").value = 20;
+        return;
+    } else if (numOfDice == 0) {
+        window.alert("You need to select the correct amount!");
+        return;
+    } 
+
+    // loop to generate values
+    for (let i = 0; i < numOfDice; i++){
+        const value = Math.floor(Math.random() * 6) + 1;
+        values.push(value);
+        images.push(`<img src="img/${value}.gif">`);
+        reloadGifs()
+    }
+    
+    // display results
+    diceResults.textContent = `The results are: ${values.join(', ')}`;
+    diceImgs.innerHTML = images.join("");
+    
+    // rename button after first roll
+    let button = document.getElementById("rollBtn").innerHTML = "Roll Again!";
+
+    // reload images
+    function reloadGifs() {
+        const diceImgs = document.getElementById("diceImgs").getElementsByTagName("img");
+        for (let img of diceImgs) {
+        img.src = img.src;
+        }
+    }
+}
---
+++ readme.md
@@ -0,0 +1,7 @@
+## Dice Roller App 🎲
+
+A minimalist web app that allows users to roll up to 20 dice simultaneously and view the results instantly.
+
+Stacks used: JavaScript, HTML and CSS.
+
+Click [here](https://slvgabriel-dice-roll-game.netlify.app/) to view the web app.
